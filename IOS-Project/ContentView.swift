407cbdd5ad45a6ff291ec7695f79c90e2fb3906f
robbedelsoir <robbe.delsoir@hotmail.be>
2022-11-18 17:32:12 +0100

    GitHub test

--- IOS-Project/ContentView.swift
+++ IOS-Project/ContentView.swift
@@ -11,11 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
     }
 }
 
@@ -24,3 +19,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+    }
