55c0693ae152b0e0f5027d229d8249c203c35e3c
robbedelsoir <robbe.delsoir@hotmail.be>
2022-12-19 22:36:15 +0100

    Een MyAccount pagina toegevoegd en daarin een 3D model proberen stoppen, dit moet nog bekeken worden

---
+++ IOS-Project/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  IOS-Project
+//
+//  Created by Robbe on 19/12/2022.
+//
+
+import SwiftUI
+
+struct Post: Identifiable{
+    var id = UUID().uuidString
+    var postImage : String
+}
---
+++ IOS-Project/Views/MyAccount.swift
@@ -0,0 +1,79 @@
+//
+//  MyAccount.swift
+//  IOS-Project
+//
+//  Created by Robbe on 19/12/2022.
+//
+
+import SwiftUI
+import UIKit
+import SceneKit
+
+struct MyAccount: View {
+    var body: some View {
+        ViewController()
+    }
+}
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1: Load .obj file
+        let scene = SCNScene(named: "converse_obj.obj")
+        
+        // 2: Add camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        // 3: Place camera
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        // 4: Set camera on scene
+        scene?.rootNode.addChildNode(cameraNode)
+        
+        // 5: Adding light to scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        scene?.rootNode.addChildNode(lightNode)
+        
+        // 6: Creating and adding ambien light to scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene?.rootNode.addChildNode(ambientLightNode)
+        
+        // If you don't want to fix manually the lights
+//        sceneView.autoenablesDefaultLighting = true
+        
+        // Allow user to manipulate camera
+        sceneView.allowsCameraControl = true
+        
+        // Show FPS logs and timming
+        // sceneView.showsStatistics = true
+        
+        // Set background color
+        sceneView.backgroundColor = UIColor.white
+        
+        // Allow user translate image
+        sceneView.cameraControlConfiguration.allowsTranslation = false
+        
+        // Set scene settings
+        sceneView.scene = scene
+        
+    }
+    
+    
+}
+
+struct MyAccount_Previews: PreviewProvider {
+    static var previews: some View {
+        MyAccount()
+    }
+}
